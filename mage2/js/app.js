a84df1f236c5abcc2e9be8dc4d24bdf6dd70f946
Ray Bogman <bogman@bogman.info>
2022-10-03 20:08:59 -0700

    rename

--- mage2/js/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-    $('.menus h3').on('click', function(e) {
-        $(this).next('ul').toggleClass('open');
-        updateHeight();
-        e.preventDefault(); return false;
-    });
-
-});
--- mage2/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-git add . && git commit -m "update Readme" && git push
-
-
